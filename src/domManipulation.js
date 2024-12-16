e7cd57287aa42d606153340dea204f63731047ae
federicotempo <federicotempo98@gmail.com>
2024-12-16 16:35:41 -0300

    Add render title and display sidebar
    This commit creates displaySidebar function and renderProjectTitle

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -3,10 +3,29 @@ const renderProjectList = (projects) => {
   projects.forEach((project) => {
     const projectTitle = document.createElement("li");
     projectTitle.textContent = project.title;
+    projectTitle.addEventListener("click", renderProjectTitle);
     projectList.appendChild(projectTitle);
   });
 };
 
+const renderProjectTitle = (title) => {
+  const projectTitle = document.querySelector("#project-title");
+  projectTitle.textContent = title.target.textContent;
+};
 
+const displaySidebar = () => {
+  const toggleButton = document.querySelector("#toggle-sidebar");
+  const sidebar = document.querySelector(".sidebar");
 
-export { renderProjectList };
+  toggleButton.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!sidebar.contains(event.target) && event.target !== toggleButton) {
+      sidebar.classList.remove("open");
+    }
+  });
+};
+
+export { renderProjectList, displaySidebar };
