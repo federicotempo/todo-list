f32df10056aed718dcda0a442f5bfdcb232cb863
federicotempo <federicotempo98@gmail.com>
2024-12-31 10:36:38 -0300

    Create cleanList, renderTodos and createTodo
    This commit creates new functions to manipulate Todos.

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -13,26 +13,53 @@ const changeProjectTitle = (project) => {
   projectTitle.textContent = project.target.textContent;
 };
 
-const chooseTodos = (projects) => {
-  console.log(projects);
+const renderTodos = (projects) => {
   const projectList = document.querySelector("#project-list");
   const listProject = projectList.querySelectorAll("li");
   listProject.forEach((li) => {
     li.addEventListener("click", () => {
       for (const project of projects) {
         if (project.title === li.textContent) {
-          console.log(project);
-          renderTodo(project);
+          createTodo(project);
         }
       }
     });
   });
 };
 
-// const renderTodo = (project) => {
-//   const taskList = document.querySelector("#task-list");
+const createTodo = (project) => {
+  const taskList = document.querySelector("#task-list");
+  cleanList(taskList);
+  project.todos.forEach((todo) => {
+    const task = document.createElement("li");
+    task.classList.add("task");
+    const taskTitle = document.createElement("div");
+    taskTitle.classList.add("task-title");
+    taskTitle.textContent = todo.title;
+    const description = document.createElement("span");
+    description.classList.add("description");
+    description.textContent = todo.description;
+    const taskDetails = document.createElement("div");
+    taskDetails.classList.add("task-details");
+    const priority = document.createElement("span");
+    priority.classList.add("priority");
+    priority.style.backgroundColor = "#ffeb3b";
+    const date = document.createElement("span");
+    date.classList.add("date");
+    date.textContent = todo.dueDate;
+    const taskDelete = document.createElement("button");
+    taskDelete.classList.add("task-delete");
+    taskDelete.textContent = "✕";
+    taskDetails.append(priority, date, taskDelete);
+    taskTitle.appendChild(description);
+    task.append(taskTitle, taskDetails);
+    taskList.appendChild(task);
+  });
+};
 
-// }
+const cleanList = (taskList) => {
+  taskList.innerHTML = "";
+}
 
 const renderProjectTitle = (projects) => {
   const projectTitle = document.querySelector("#project-title");
@@ -71,5 +98,5 @@ export {
   displaySidebar,
   hideSidebar,
   renderProjectTitle,
-  chooseTodos,
+  renderTodos,
 };
