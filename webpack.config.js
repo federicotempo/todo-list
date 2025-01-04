c0365590133bf48809f39481f79bb64d2334f3b0
federicotempo <federicotempo98@gmail.com>
2025-01-04 14:45:47 -0300

    Change Public Path
    This commit changes public path.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/todo-list/',
   },
   devtool: "eval-source-map",
   devServer: {
